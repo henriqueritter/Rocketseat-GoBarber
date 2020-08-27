41faf5e05666979f038176eec46ede2bf9d3e931
Henrique Ritter <henrique-ritter@hotmail.com>
2020-08-27 16:46:01 -0300

    Update Server.ts

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -47,5 +47,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.log('🚀 Server started on port 3333');
 });
